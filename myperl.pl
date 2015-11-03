d72c975274f0f6fd59d86d9ccfa8cec3e51a4fc2
bakelitebob <bakelitebob@gmail.com>
2015-11-02 20:13:47 -0500

    my first commit

---
+++ myperl.pl
@@ -0,0 +1,23 @@
+#!/usr/local/bin/perl -w
+use strict;
+use warnings;
+print " \n"; #remember to put a semicolon after the command to show command is 
+finished
+print "Hello world\n";
+print 42, "\n";
+print " \n";
+print "One Fish,\n";
+print "Two Fish,\n";
+print "Red Fish,\n";
+print "Blue Fish.\n";
+print " \n";
+print "5 + 6 = ", 5+6, "\n";
+print "(2 + 3) * 6 = ", (2+3)*6, "\n";
+print "2 + 3 * 6 = ", 2+3*6, "\n";
+print "2 raised to the power of 8 is ", 2**8, "\n";
+print "10-5 = ", 10-5, ". 5-10 = ", 5-10, "\n";
+print "2/3 = ", 2/3, "\n";
+print " \n";
+print "Hello," . " " . "World!" . "\n" .
+    "And this is the second line.\n";
+print " \n";
